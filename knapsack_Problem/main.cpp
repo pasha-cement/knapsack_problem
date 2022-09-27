f0af7057a1132fb128ce823b548640e42a2a0abc
pasha-cement <kolesnik-pavel-fsr@yandex.ru>
2022-09-27 16:38:04 +0300

    first commit

---
+++ knapsack_Problem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  knapsack_Problem
+//
+//  Created by Павел on 27.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
