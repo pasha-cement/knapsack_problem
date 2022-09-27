64aa2b2c937ff4e749f24be311f076320530dca8
pasha-cement <kolesnik-pavel-fsr@yandex.ru>
2022-09-27 16:52:58 +0300

    add some code

--- knapsack_Problem/main.cpp
+++ knapsack_Problem/main.cpp
@@ -1,14 +1,59 @@
-//
-//  main.cpp
-//  knapsack_Problem
-//
-//  Created by Павел on 27.09.2022.
-//
-
+#include <vector>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+namespace fs = filesystem;
+
+int solve(int n, vector<int> values, vector<int> weights, int W) {
+    vector<int> memo1(W+1, 0);
+    vector<int> memo2(W+1, 0);
+    for (int i = 1; i <= n; i++) {
+        vector<int> temp = memo1;
+        memo1 = memo2;
+        memo2 = temp;
+        for (int j = 0; j <= W; j++) {
+            memo2[j] = memo1[j];
+            if (j - weights[i - 1] >= 0) {
+                memo2[j] = max(memo2[j], memo1[j - weights[i-1]] + values[i-1]);
+            }
+        }
+    }
+    int result = 0;
+    for (int i = W; i >= 0; i--) {
+        if(memo2[i] != 0) {
+            result = memo2[i];
+            break;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string path = "/Users/pavel/Desktop/knapsack_problem/data";
+       auto it = fs::directory_iterator(path);
+       vector<fs::path> array_path;
+       copy_if(fs::begin(it), fs::end(it), std::back_inserter(array_path),
+           [](const auto& entry) {
+               return fs::is_regular_file(entry);
+       });
+    for (auto& p : array_path) {
+        ifstream fin;
+        fin.open(p.string());
+        cout << p.string() << endl;
+        int n, W;
+        fin >> n >> W;
+        vector<int> values;
+        vector<int> weights;
+        for (int i = 0; i < n; i++) {
+            int value, weight;
+            fin >> value >> weight;
+            values.push_back(value);
+            weights.push_back(weight);
+        }
+        int max_value = solve(n, values, weights, W);
+        cout << max_value << endl;
+    }
     return 0;
 }
